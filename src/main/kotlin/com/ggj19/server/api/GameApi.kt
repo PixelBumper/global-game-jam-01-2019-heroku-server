9f11b864bd1fa3dcb7ae871af46d698d5646ea77
pixelbumper <PixelBumper@users.noreply.github.com>
2019-01-26 12:06:12 +0100

    fixed api

--- src/main/kotlin/com/ggj19/server/api/GameApi.kt
+++ src/main/kotlin/com/ggj19/server/api/GameApi.kt
@@ -9,6 +9,7 @@ import com.ggj19.server.dtos.RoomState.Room
 import com.ggj19.server.dtos.asRoomInformation
 import com.github.javafaker.Faker
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.stereotype.Component
@@ -29,74 +30,86 @@ import javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED
 @OpenAPIDefinition(info = Info(title = "Game Server", version = "1.0.0"))
 @Tag(name = "GameApi")
 class GameApi {
-  private val faker = Faker()
-  private val rooms = HashMap<RoomName, RoomState>()
+    private val faker = Faker()
+    private val rooms = HashMap<RoomName, RoomState>()
 
-  @POST @Path("/start-room") @Consumes(APPLICATION_FORM_URLENCODED) fun startRoom(
-    @FormParam("playerId") playerId: PlayerId,
-    @FormParam("roomName") roomName: RoomName
-  ): RoomInformation {
-    val state: RoomState
-
-    synchronized(rooms) {
-      state = rooms[roomName] ?: throw IllegalArgumentException("Can't find a room with the name: $roomName")
-    }
-
-    when (state) {
-      is Room -> {
-        require(state.owner != playerId) { "You're not the owner of the room and hence can't start the room" }
+    @GET
+    @Path("/start-room")
+    @Operation(operationId = "startRoom")
+    fun startRoom(
+            @QueryParam("playerId") playerId: PlayerId,
+            @QueryParam("roomName") roomName: RoomName
+    ): RoomInformation {
+        val state: RoomState
 
         synchronized(rooms) {
-          rooms[roomName] = Playing(state.players)
+            state = rooms[roomName] ?: throw IllegalArgumentException("Can't find a room with the name: $roomName")
         }
-      }
-      is Playing -> {
-        throw IllegalArgumentException("Game has already started")
-      }
+
+        when (state) {
+            is Room -> {
+                require(state.owner != playerId) { "You're not the owner of the room and hence can't start the room" }
+
+                synchronized(rooms) {
+                    rooms[roomName] = Playing(state.players)
+                }
+            }
+            is Playing -> {
+                throw IllegalArgumentException("Game has already started")
+            }
+        }
+
+        return state.asRoomInformation()
     }
 
-    return state.asRoomInformation()
-  }
+    @GET
+    @Path("/create-room")
+    @Operation(operationId = "createRoom")
+    fun createRoom(
+            @QueryParam("playerId") playerId: PlayerId
+    ): Room {
+        val roomName = RoomName(faker.name().fullName()) // For the Game Jam we will assume we won't clash and override a room with the same name.
+        val room = RoomState.Room(roomName, playerId, listOf(playerId))
 
-  @POST @Path("/create-room") @Consumes(APPLICATION_FORM_URLENCODED) fun createRoom(
-    @FormParam("playerId") playerId: PlayerId
-  ): Room {
-    val roomName = RoomName(faker.name().fullName()) // For the Game Jam we will assume we won't clash and override a room with the same name.
-    val room = RoomState.Room(roomName, playerId, listOf(playerId))
+        synchronized(rooms) {
+            rooms[roomName] = room
+        }
 
-    synchronized(rooms) {
-      rooms[roomName] = room
+        return room
     }
 
-    return room
-  }
+    @GET
+    @Path("/join-room")
+    @Operation(operationId = "joinRoom")
+    fun joinRoom(
+            @QueryParam("playerId") playerId: PlayerId,
+            @QueryParam("roomName") roomName: RoomName
+    ): RoomInformation {
+        val room: RoomState
 
-  @POST @Path("/join-room") @Consumes(APPLICATION_FORM_URLENCODED) fun joinRoom(
-    @FormParam("playerId") playerId: PlayerId,
-    @FormParam("roomName") roomName: RoomName
-  ): RoomInformation {
-    val room: RoomState
+        synchronized(rooms) {
+            room = rooms[roomName] ?: throw IllegalArgumentException("Can't find a room with the name: $roomName")
 
-    synchronized(rooms) {
-      room = rooms[roomName] ?: throw IllegalArgumentException("Can't find a room with the name: $roomName")
+            if (room.players.contains(playerId)) {
+                throw IllegalArgumentException("You are already part of the room with the name: $roomName")
+            }
 
-      if (room.players.contains(playerId)) {
-        throw IllegalArgumentException("You are already part of the room with the name: $roomName")
-      }
+            rooms.put(roomName, room.copyJoining(playerId))
+        }
 
-      rooms.put(roomName, room.copyJoining(playerId))
+        return room.asRoomInformation()
     }
 
-    return room.asRoomInformation()
-  }
+    @GET
+    @Path("/room-information")
+    @Operation(operationId = "roomInformation")
+    fun roomInformation(@QueryParam("roomName") roomName: RoomName): RoomInformation {
+        val room: RoomState
 
-  @GET @Path("/room-information") fun roomInformation(@QueryParam("roomName") roomName: RoomName): RoomInformation {
-    val room: RoomState
+        synchronized(rooms) {
+            room = rooms[roomName] ?: throw IllegalArgumentException("Can't find a room with the name: $roomName")
+        }
 
-    synchronized(rooms) {
-      room = rooms[roomName] ?: throw IllegalArgumentException("Can't find a room with the name: $roomName")
+        return room.asRoomInformation()
     }
-
-    return room.asRoomInformation()
-  }
 }
